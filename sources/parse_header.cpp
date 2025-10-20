026a1a25d65cf4e120711e458f62b7fb63c6d459
Clara Bordeau <cbordeau@f4r2s2.clusters.42paris.fr>
2025-10-20 16:23:24 +0200

    parsing

--- sources/parse_header.cpp
+++ sources/parse_header.cpp
@@ -6,20 +6,38 @@
 /*   By: cbordeau <cbordeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 13:43:32 by cbordeau          #+#    #+#             */
-/*   Updated: 2025/10/20 15:28:53 by cbordeau         ###   ########.fr       */
+/*   Updated: 2025/10/20 16:11:34 by cbordeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/epoll.h>
 #include <string>
 #include <iostream>
+#include "Request.hpp"
 
 const std::string DCRLF = "\r\n\r\n";
 const std::string CRLF = "\r\n";
 
-void	parse_buffer(std::string buffer, struct epoll_event * event)
+int	move_cursor(std::string::size_type *cursor, std::string str, std::string toFind)
 {
-	int cursor = 0;
+	*cursor = str.find(toFind);
+	if (*cursor != std::string::npos)
+		return 1;
+	else
+		return 0;
+}
+void	parse_buffer(std::string buffer, Request *request)
+{
+	std::string::size_type cursor = 0;
+	if (move_cursor(&cursor, buffer, DCRLF) && !request->get_hEnd())
+	{
+		request->appendHeader(buffer, 0, cursor);
+		request->set_hEnd(1);
+		buffer.erase(0, cursor + 2);
+		if (move_cursor(&cursor, buffer, DCRLF))
+		{
+		}
+	}
 }
 
 void	get_token(std::string *header, std::string *token, std::string::size_type *cursor)
