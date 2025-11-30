30942b70d649de7b616996bbcf242a1ca73eb4bd
Aykrifa <aykrifa@student.42.fr>
2025-11-30 16:07:59 +0100

    added fd + listen method

--- code/classes/Server/Server.cpp
+++ code/classes/Server/Server.cpp
@@ -11,11 +11,20 @@
 
 #include "Server.hpp"
 #include "Location.hpp"
+#include "Logger.hpp"
+#include <stdexcept>
 
-Server::Server(void): _port(8080), _interface(0)
+Server::Server(void):
+	_port(8080),
+	_interface(0),
+	_fd(-1)
 {}
 
-Server::Server(const Server &copy): _locations(copy._locations), _port(copy.getPort()), _interface(copy._interface)
+Server::Server(const Server &copy):
+	_locations(copy._locations),
+	_port(copy.getPort()),
+	_interface(copy._interface),
+	_fd(-1)
 {}
 
 Server::~Server(void)
@@ -28,6 +37,7 @@ Server	Server::operator=(const Server &rhs)
 	this->setLocationsMap(rhs.getLocations());
 	this->setPort(rhs.getPort());
 	this->setInterface(rhs.getInterface());
+	this->setFd(-1);
 	return (*this);
 }
 
@@ -35,13 +45,14 @@ const std::map<std::string, Location>	&Server::getLocations(void) const
 {
 	return (this->_locations);
 }
-
+// copy map into object
 void	Server::setLocationsMap(const std::map<std::string, Location>	&locations)
 {
 	this->_locations = locations;
 }
-
-void	Server::addLocations(std::string &key, Location &value)
+// take string as key and try to push a location,
+// if key alaready exists, throw
+void	Server::addLocations(const std::string &key, const Location &value)
 {
 	if (this->_locations.find(key) != this->_locations.end())
 		throw (std::runtime_error("URL already exist"));
@@ -66,4 +77,49 @@ unsigned int	Server::getInterface(void) const
 void	Server::setInterface(unsigned int interface)
 {
 	this->_interface = interface;
+}
+
+int		Server::getFd(void) const
+{
+	return (this->_fd);
+}
+
+void	Server::setFd(const int fd)
+{
+	this->_fd = fd;
+}
+
+void	Server::startListen(void)
+{
+
+    // 1. Créer le socket serveur
+	/* Create a new socket of type TYPE in domain DOMAIN, using
+   protocol PROTOCOL.  If PROTOCOL is zero, one is chosen automatically.
+   Returns a file descriptor for the new socket, or -1 for errors.  */
+    this->setFd(socket(this->socket_adress_family, this->socket_type, this->socket_protocol));
+    if (this->getFd() == -1)
+	{
+		throw (std::runtime_error("socket"));
+    }
+    // Configurer le socket pour le réutiliser rapidement
+    int opt = 1;
+    setsockopt(this->getFd(), SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    // Lier le socket à une adresse et un port
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = this->socket_type;
+    server_addr.sin_addr.s_addr = htonl(this->getInterface());
+    server_addr.sin_port = htons(this->getPort());
+    if (bind(this->getFd(), (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+	{
+		throw (std::runtime_error("bind"));
+    }
+
+	Logger::print(LOG_SERVER) << "htons: " << server_addr.sin_port << std::endl;
+
+    // Écouter les connexions entrantes
+    if (listen(this->getFd(), this->socket_max_connection) == -1)
+	{
+		throw (std::runtime_error("listen"));
+    }
 }
--- code/classes/Server/Server.hpp
+++ code/classes/Server/Server.hpp
@@ -23,20 +23,41 @@ class	Server
 		~Server(void);
 		Server	operator=(const Server&);
 
-		static const int				adress_family = AF_INET;
+	// man socket
+		// The domain argument specifies a communication domain; this selects the protocol family which will be used for communication.
+		static const int				socket_adress_family = AF_INET;
+	// man socket
+		// The socket has the indicated type, which specifies the communication semantics.
+		static const int				socket_type = SOCK_STREAM;
+	// man socket
+		// The  protocol  specifies  a  particular protocol to be used with the socket.
+		// Normally only a single protocol exists to support a particular socket type within a given protocol family, in which case
+		//    protocol can be specified as 0.
+		// However, it is possible that many protocols may exist, in which case a particular protocol must be specified in this manner.
+		// The protocol number to use is  specific to the “communication domain” in which communication is to take place;
+		// see protocols(5).
+		static const int				socket_protocol = 0;
+	//
+		// max connection until listen will fail
+		static const int				socket_max_connection = SOMAXCONN;
 
 		const std::map<std::string, Location>	&getLocations(void) const;
 		void									setLocationsMap(const std::map<std::string, Location>&);
-		void									addLocations(std::string&, Location&);
+		void									addLocations(const std::string&, const Location&);
 
 		unsigned short int						getPort(void) const;
 		void									setPort(unsigned short int);
 
 		unsigned int							getInterface(void) const;
 		void									setInterface(unsigned int);
+
+		int										getFd(void) const;
+		void									setFd(const int);
 	
+		void									startListen(void);
 	private:
 		std::map<std::string, Location>	_locations;
 		unsigned short int				_port;
 		unsigned int					_interface;
+		int								_fd;
 };
